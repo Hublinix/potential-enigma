b56200472b83968c6586218b86999caf94eb4de8
Kachi <kachinwosu1999@gmail.com>
2024-08-05 14:25:33 -0500

    Updated Changes

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,51 @@
 // TODO: Include packages needed for this application
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const inquirer = require('inquirer');
+const fs = require("fs");
 
+// TODO: Create an array of questions for user input
+const questions = ["What is your movtivation?","Why did you build this project?","What problem does it solve?","What did you learn?"];
+
+inquirer
+    .questions([
+        {
+            type: "input",
+            message:"What is your movtivation?",
+            question: "Motive",
+    
+        },
+
+        {
+            type: "input",
+            message:"Why did you build this project?",
+            question: "Project",
+    
+        },
+
+        {
+            type: "input",
+            message:"What problem does it solve?",
+            question: "Problem",
+    
+        },
+
+        {
+            type: "input",
+            message:"What did you learn?",
+            question: "Learning",
+    
+        },
+    ])
+    .then((answers) => {
+        fs.writeFileSync("Thank you for answering out questions the README file will start soon.",JSON.stringify(answers,null,'\t'),(err)=>{
+            console.error(err)
+        })
+    })
+    .catch(err => console.error(err));
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+}
 
 // TODO: Create a function to initialize app
 function init() {}
